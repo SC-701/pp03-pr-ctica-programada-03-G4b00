2b3224037fbf66fbb1cedb6432c1d27f2848b9e6
Gabriel Chaves <gchavesrojas@gmail.com>
2026-03-19 00:01:19 -0600

    Practica 3

---
+++ Vehiculo.Web/Web/Pages/Vehiculos/Eliminar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Vehiculos
+{
+    public class EliminarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
